cd823faf41644bcf7d86354bb72e7b5b7a4cc1ca
Paige C Willey <paige.catherine.willey@gmail.com>
2016-12-12 12:44:11 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//app.js
+
+new Vue({
+//We want to target the div with an id of 'events'
+	el: '#events' 
+
+	// Here we can register any values or collections that hold data
+	// for the application
+	data: {},
+
+	// Anything within the ready function will run when the application loads
+	ready: function() {},
+});
